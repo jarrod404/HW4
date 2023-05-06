324a0f293efee109e94518e358e602738857695d
Jarrod Rogers <jtr_gamer@hotmail.com>
2023-05-05 22:18:55 -0500

    All tests now pass

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -1,6 +1,10 @@
 public class Average {
+	/*
+	 * Computes the average of an array of integers given the array's length
+	 */
 	public static int average(int k, int[] list) {
 		int average = 0;
+		k = Math.abs(k);
 		int n = Math.min(k, list.length);
 		if (n > 0) {
 			for(int i = 0; i < n; i++) {
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -3,9 +3,37 @@ import static org.junit.Assert.*;
 
 public class SimpleTest {
 	@Test
-	public void testAverage() {
+	public void testAveragePositive() {
 		int[] numbers = {10, 20, 30, 40, 50};
 		int avg = Average.average(5, numbers);
-		assertEquals(avg,  30);
+		assertEquals(30, avg);
+	}
+
+	@Test
+	public void testAverageNegative() {
+		int[] numbers = {-10, 20, -30, 40, -50};
+		int avg = Average.average(5, numbers);
+		assertEquals(-6, avg);
+	}
+
+	@Test
+	public void testAverageZeros() {
+		int[] numbers = {0, 0, 0, 0, 0};
+		int avg = Average.average(5, numbers);
+		assertEquals(0, avg);
+	}
+
+	@Test
+	public void testAverageSingleNumber() {
+		int[] numbers = {4};
+		int avg = Average.average(1, numbers);
+		assertEquals(4, avg);
+	}
+
+	@Test
+	public void testAverageNegativeSize() {
+		int[] numbers = {10, 20, 30, 40, 50};
+		int avg = Average.average(-5, numbers);
+		assertEquals(30, avg);
 	}
 }
